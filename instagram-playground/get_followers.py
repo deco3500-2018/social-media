0c9c8444fb1de3baf42bec3effc687f077a9de64
darren_fu <d.fu@uq.edu.au>
2018-10-02 19:34:06 +1000

    First commit: can get followers of any public account

---
+++ instagram-playground/get_followers.py
@@ -0,0 +1,24 @@
+from instagram_private_api import Client, ClientCompatPatch
+import sys
+
+# check that username is given
+if len(sys.argv) < 2:
+    sys.exit();
+
+print('Looking for followings by ' + sys.argv[1] + '...')
+
+# login
+user_name = ""
+password = ""
+with open('login.txt', 'r') as file:
+    user_name = file.readline()
+    password = file.readline()
+api = Client(user_name, password)
+
+# get user and follows
+user_id = api.username_info(sys.argv[1])['user']['pk']
+follows = api.user_following(user_id, api.generate_uuid())
+
+# print follows
+for user in follows['users']:
+        print(user['username'])
---
+++ instagram-playground/readme.md
@@ -0,0 +1,8 @@
+# Installation
+
+Follow instructions here:
+https://instagram-private-api.readthedocs.io/en/latest/usage.html
+
+In this folder, create a file named login.txt. Write an instagram login on the first line, write the password on the second line.
+
+Don't use a real account.
