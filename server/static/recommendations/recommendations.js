8f11760c123f7175248368e0ff16c80d9138462f
darren_fu <d.fu@uq.edu.au>
2018-10-23 11:31:00 +1000

    Now interactive based on data

--- server/static/recommendations/recommendations.js
+++ server/static/recommendations/recommendations.js
@@ -1,5 +1,152 @@
 window.onload = init;
 
+let data = {
+    "users": [
+        {
+            "name": "Darren Fu",
+            "username": "john.mturk",
+            "percentage": 86,
+            "postsPerWeek": 15,
+            "averageLikes": 247,
+            "profilePic": "/assets/test_profile_pic.jpg",
+            "photos": [
+                "/assets/photo.png",
+                "/assets/photo.png",
+                "/assets/photo.png",
+                "/assets/photo.png",
+                "/assets/photo.png"
+            ]
+        },
+
+        {
+            "name": "Jacob Ellis",
+            "username": "jellis_much",
+            "percentage": 50,
+            "postsPerWeek": 28,
+            "averageLikes": 341,
+            "profilePic": "/assets/test_profile_pic.jpg",
+            "photos": [
+                "/assets/photo.png",
+                "/assets/photo.png",
+                "/assets/photo.png",
+                "/assets/photo.png",
+                "/assets/photo.png"
+            ]
+        },
+
+        {
+            "name": "Edwin Matthews",
+            "username": "matty_e6",
+            "percentage": 60,
+            "postsPerWeek": 4,
+            "averageLikes": 111,
+            "profilePic": "/assets/test_profile_pic.jpg",
+            "photos": [
+                "/assets/photo.png",
+                "/assets/photo.png",
+                "/assets/photo.png",
+                "/assets/photo.png",
+                "/assets/photo.png"
+            ]
+        },
+
+        {
+            "name": "Andrew Davies",
+            "username": "ilovedeco3500",
+            "percentage": 74,
+            "postsPerWeek": 100,
+            "averageLikes": 118,
+            "profilePic": "/assets/test_profile_pic.jpg",
+            "photos": [
+                "/assets/photo.png",
+                "/assets/photo.png",
+                "/assets/photo.png",
+                "/assets/photo.png",
+                "/assets/photo.png"
+            ]
+        }
+    ]
+};
+
 function init() {
-    // alert('recommend you leave');
+    getData().then(updateFollowList);
+}
+
+function getData() {
+    localStorage.setItem('recommendations', JSON.stringify(data));
+    return Promise.resolve(data);
+}
+
+function updateFollowList(data) {
+    let followingList = document.querySelector('#following-list');
+    
+    for (user of data.users) {
+        let followingElem = document.createElement('div');
+        followingElem.setAttribute('class', 'follow');
+        followingElem.setAttribute('username', user.username);
+        followingElem.innerHTML = `
+            <img src="${ user.profilePic }">
+            <div class="names">
+                <h4>${ user.name }</h4>
+                <h4>${ user.username }</h4>
+            </div>
+            <div class="decision">
+                <i class="fas fa-check" onclick="tickClickHandler"></i>
+                <i class="fas fa-times" onclick="rejectClickHandler"></i>
+            </div>
+        `;
+        followingList.appendChild(followingElem);
+        followingElem.addEventListener('click', event => {
+            document.querySelector("#follow-detail").style.opacity = '0';
+            setTimeout(() => {
+                populateFollowDetail(event.target.getAttribute('username'));
+                document.querySelector("#follow-detail").style.opacity = '1';
+            }, 250);
+        }, false);
+    }
+
+    // select first in the list
+    populateFollowDetail(data.users[0].username);
+}
+
+function tickClickHandler(followingElm) {
+    alert('Add');
+}
+
+function rejectClickHanlder(followingElm) {
+    alert('Remove');
+    // remove from dom
+
+    // remove from data
+}
+
+function populateFollowDetail(username) {
+    let user = getRecommendations().users.find(user => user.username === username);
+
+    if (!user) {
+        return;
+    }
+
+    let followDetail = document.querySelector('#follow-detail');
+    followDetail.innerHTML = `
+        <div class="heading">
+            <h1>${ user.name.split(" ")[0] } followers are</h1>
+            <h1 class="large">${ user.percentage }%</h1>
+            <h1>more likely to be sadder.</h1>
+        </div>
+
+        <h1 class="post-count"><span class="dark">${ user.postsPerWeek }</span> posts per week</h1>
+
+        <h1><span class="dark">${ user.averageLikes }</span> likes on average</h1>
+
+        <div class="gallery">
+            ${
+                user.photos.map( photo => `<img src=${photo}>`).join('')
+            }
+        </div>
+    `;
+}
+
+function getRecommendations() {
+    return JSON.parse(localStorage.getItem('recommendations'));
 }
